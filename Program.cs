50b3fad737fcaa3986db5bc365cc6d6e635aaad6
PREDATOR <PREDATOR@LAPTOP-7P5CDOI4>
2022-05-03 16:05:12 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ListeDongu1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "java" , "c#", "python" ,"sql" , "html" ,"go lang" , " flutter"} ;
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+
+            }
+
+            foreach (string kurs  in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            {
+
+            }
+            {
+
+            }
+                
+            
+            {
+                
+            }
+            
+
+
+        }
+    }
+}
